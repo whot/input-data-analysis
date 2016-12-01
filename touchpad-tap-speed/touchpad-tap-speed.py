5d3741d007a60cc7225f7dba3ebc6f2cd2ab1c65
Peter Hutterer <peter.hutterer@who-t.net>
2016-12-01 16:47:27 +1000

    touchpad-tap-speed: use location data too

--- touchpad-tap-speed/touchpad-tap-speed.py
+++ touchpad-tap-speed/touchpad-tap-speed.py
@@ -22,6 +22,8 @@ class TouchpadTapSpeed(EventProcessor):
                             default=6,
                             help="Ignore sequences moving more than X mm (default 6)")
         parser.add_argument("--sort-by-mm", action="store_true", help="Sort by mm rather than time")
+        parser.add_argument("--use-location", action="store_true",
+                            help="Only print the start location for each tap")
 
     def process_one_file(self, f, args):
         self.gnuplot.comment("processing {}".format(f))
@@ -32,6 +34,7 @@ class TouchpadTapSpeed(EventProcessor):
 
         mm = [ 0 ] * (args.max_move + 1) * 10
         times = [ 0 ] * (args.max_time + 1)
+        locations = []
 
         for s in singles:
             first = s.first
@@ -46,21 +49,28 @@ class TouchpadTapSpeed(EventProcessor):
             if dist > args.max_move:
                 continue
 
+            # ignore the left/right 10% of the touchpad, could be palms or
+            # edge scroll
+            if first.percent[0] > 0.90 or first.percent[0] < 0.10:
+                continue
+
             mm[int(dist * 10)] += 1
-
             times[ms] += 1
+            locations.append(first.percent)
 
-        return times, mm
+        return times, mm, locations
 
     def process(self, args):
         times = [ 0 ] * (args.max_time + 1)
         mm = [ 0 ] * (args.max_move + 1) * 10
+        locations = []
 
         for f in self.sourcefiles:
             try:
-                t, m = self.process_one_file(f, args)
+                t, m, l = self.process_one_file(f, args)
                 times = map(lambda x, y : x + y, times, t)
                 mm = map(lambda x, y : x + y, mm, m)
+                locations += l
             except DeviceError as e:
                 print("Skipping {} with error: {}".format(f, e))
         
@@ -71,6 +81,11 @@ class TouchpadTapSpeed(EventProcessor):
             self.gnuplot.labels("movement distance (in 0.1mm)", "count")
             for mm, count in enumerate(mm):
                 self.gnuplot.data("{} {}".format(mm, count))
+        elif args.use_location:
+            self.gnuplot.labels("x", "y")
+            self.gnuplot.ranges("0:100", "0:100")
+            for x, y in locations:
+                self.gnuplot.data("{} {}".format(x * 100, y * 100))
         else:
             self.gnuplot.labels("press-release time (ms)", "count")
             for ms, count in enumerate(times):
